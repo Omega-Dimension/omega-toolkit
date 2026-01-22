411f651ecd1012f599cbfa95a391fb765d83f008
ScorpioFento <scorpiofento@gmail.com>
2026-01-22 11:32:31 +0630

    fix: semantic tag in sidebar

--- src/layout/Sidebar/MenuList.tsx
+++ src/layout/Sidebar/MenuList.tsx
@@ -25,27 +25,32 @@ export default function MenuList({
       component="nav"
       sx={{ flex: 1, overflowY: "auto", p: collapsed ? 2 : 2.5 }}
     >
-      {items.map((item, index) => (
-        <Box
-          key={index}
-          onMouseEnter={(e) => collapsed && onItemHover(item, e.currentTarget)}
-          onMouseLeave={onItemLeave}
-        >
-          {collapsed ? (
-            <CollapsedMenuItem
-              item={item}
-              onClose={isMobile ? onClose : undefined}
-              onHover={(e) => onItemHover(item, e.currentTarget)}
-              onLeave={onItemLeave}
-            />
-          ) : (
-            <ExpandedMenuItem
-              item={item}
-              onClose={isMobile ? onClose : undefined}
-            />
-          )}
-        </Box>
-      ))}
+      <Box component="ul" sx={{ listStyle: "none", p: 0, m: 0 }}>
+        {items.map((item, index) => (
+          <Box
+            component="li"
+            key={index}
+            onMouseEnter={(e) =>
+              collapsed && onItemHover(item, e.currentTarget)
+            }
+            onMouseLeave={onItemLeave}
+          >
+            {collapsed ? (
+              <CollapsedMenuItem
+                item={item}
+                onClose={isMobile ? onClose : undefined}
+                onHover={(e) => onItemHover(item, e.currentTarget)}
+                onLeave={onItemLeave}
+              />
+            ) : (
+              <ExpandedMenuItem
+                item={item}
+                onClose={isMobile ? onClose : undefined}
+              />
+            )}
+          </Box>
+        ))}
+      </Box>
     </List>
   );
 }
