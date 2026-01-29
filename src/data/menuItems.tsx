61c0432d2644f352a4c80b0ef65c8cd4103fa980
ScorpioFento <scorpiofento@gmail.com>
2026-01-29 15:31:02 +0630

    configure: route for fav icon generator

--- src/data/menuItems.tsx
+++ src/data/menuItems.tsx
@@ -38,6 +38,7 @@ export const menuItems: MenuItemProps[] = [
     children: [
       { label: "Jpg to Png Image", path: "/image/jpg-png" },
       { label: "Resize Image", path: "/image/resize-image" },
+      { label : "Fav Generator", path : "/image/favicon-generator"}
     
     ],
   },
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,7 @@ import ExcelToCsv from "../pages/Files/ExcelToCsvPage";
 import JsonFormatter from "../pages/Files/JsonFormatterPage";
 import JsonToCsvPage from "../pages/Files/JsonToCsvPage";
 import Home from "../pages/Home";
+import FaviconGeneratorPage from "../pages/Images/FaviconGeneratorPage";
 import ImageResizePage from "../pages/Images/ImageResize";
 import JpgToPng from "../pages/Images/JpgToPngPage";
 
@@ -26,9 +27,9 @@ export const routes: RouteItem[] = [
       { path: "/file/excel-csv", element: <ExcelToCsv /> },
       { path: "/file/csv-excel", element: <CsvToExcelPage /> },
 
-
       { path: "/image/jpg-png", element: <JpgToPng /> },
       { path: "/image/resize-image", element: <ImageResizePage /> },
+      { path: "/image/favicon-generator", element: <FaviconGeneratorPage /> },
     ],
   },
 ];
