e6d3a118bf5b7e75c8fea23122ab3651245ef55d
ScorpioFento <scorpiofento@gmail.com>
2026-02-01 13:47:44 +0630

    add: hash generator route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,5 @@
 import MainLayout from "../layout/MainLayout";
+import HashGeneratorPage from "../pages/Dev/HashGeneratorPage";
 import CsvToExcelPage from "../pages/Files/CsvToExcelPage";
 import CsvToJsonPage from "../pages/Files/CsvToJsonPage";
 import ExcelToCsv from "../pages/Files/ExcelToCsvPage";
@@ -35,8 +36,10 @@ export const routes: RouteItem[] = [
       { path: "/image/favicon-generator", element: <FaviconGeneratorPage /> },
       { path: "/image/image-crop", element: <ImageCropPage /> },
       { path : "/image/image-base64", element : <ImageToBase64Page />},
-      { path : "image/base64-image", element : <Base64ToImagePage />},
+      { path : "/image/base64-image", element : <Base64ToImagePage />},
 
+
+      {path : "/dev/hash-generator", element : <HashGeneratorPage />},
     ],
   },
 ];
