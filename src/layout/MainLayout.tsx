cbc37dbaea78f2aed5118cda5eb1bcb79763cec5
ScorpioFento <scorpiofento@gmail.com>
2026-02-16 10:21:03 +0630

    add: animation in each page

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,14 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Container } from "@mui/material";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 export default function MainLayout() {
+
+  const pageRef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    if(!pageRef.current) return;
+
+    gsap.fromTo(
+      pageRef.current,
+      {y: 30, opacity : 0},
+      {
+        y : 0,
+        opacity : 1,
+        duration : 0.6,
+        ease : "power3.out"
+      }
+    )
+  }, [location.pathname])
+
   return (
     <>
       <Header />
   
-      <Container component="main" maxWidth="xl" sx={{ mt: 4, mb: 6 }}>
+      <Container ref = {pageRef} component="main" maxWidth="xl" sx={{ mt: 4, mb: 6 }}>
         <Outlet />
       </Container>
 
