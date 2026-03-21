8dd3320c15603340fdcd19f3e4becc981aad71cc
fento <scorpiofento@gmail.com>
2026-03-21 19:56:25 +0630

    fixed: layout height

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,10 +1,9 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
-
 export default function MainLayout() {
   const pageRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
@@ -20,24 +19,34 @@ export default function MainLayout() {
         opacity: 1,
         duration: 0.6,
         ease: "power3.out",
-      },
+      }
     );
   }, [location.pathname]);
 
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh", 
+      }}
+    >
       <Header />
 
       <Container
         ref={pageRef}
         component="main"
         maxWidth="xl"
-        sx={{ mt: 4, mb: 6 }}
+        sx={{
+          mt: 4,
+          mb: 6,
+          flex: 1, 
+        }}
       >
         <Outlet />
       </Container>
 
       <Footer />
-    </>
+    </Box>
   );
 }
